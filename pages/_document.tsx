8370532eaac15ff1ddac2c30b89fd89b1269c982
Muhammad Fayyaz <muhammad.fayyaz3@gmail.com>
2024-11-29 13:19:16 +0500

    Language Translator

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, { Head, Html, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default class MyDocument extends Document {
   render() {
@@ -13,8 +14,15 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/icon?family=Material+Icons"
             rel="stylesheet"
           />
+          <Script src="/lang-config.js" strategy="beforeInteractive" />
+          <Script src="/translation.js" strategy="beforeInteractive" />
+          <Script
+            src="//translate.google.com/translate_a/element.js?cb=TranslateInit"
+            strategy="afterInteractive"
+          />
         </Head>
         <body style={{ margin: 0 }}>
+          <div id="google_translate_element" />
           <Main />
           <NextScript />
         </body>
